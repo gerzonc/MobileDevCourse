9b72daa64d3ce06dcc3aaf51e50e6960f2d877f2
gerzonc <gerzonzcanario@gmail.com>
2019-09-06 03:01:00 -0400

    initial commit

---
+++ LoginPage/LoginPage/ViewModels/SignInPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using LoginPage.Models;
+using System.Windows.Input;
+using LoginPage.Controls;
+using Xamarin.Forms;
+using System.ComponentModel;
+
+namespace LoginPage.ViewModels
+{
+    public class SignInViewModel : INotifyPropertyChanged
+    {
+        public string fieldValidate { get; set; }
+        public ICommand LoginCommand { get; set; }
+        public User user { get; set; } = new User();
+
+        public SignInViewModel()
+        {
+
+            LoginCommand = new Command(async () => {
+
+                if (string.IsNullOrWhiteSpace(user.Username) ||  string.IsNullOrWhiteSpace(user.Password))
+                    fieldValidate = "Fields can't be empty";
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ LoginPage/LoginPage/ViewModels/SignUpPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using LoginPage.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace LoginPage.ViewModels
+{
+    public class SignUpPageViewModel : INotifyPropertyChanged
+    {
+        public string fieldValidate { get; set; }
+        public ICommand SignupCommand { get; set; }
+        public User user { get; set; } = new User();
+
+        public SignUpPageViewModel()
+        {
+            fieldValidate = "";
+
+            SignupCommand = new Command(async () => {
+
+                if (string.IsNullOrWhiteSpace(user.Password) || string.IsNullOrWhiteSpace(user.ConfirmPassword))
+                    fieldValidate = "You can't leave empty fields!";
+
+                else if (!user.ConfirmPassword.Equals(user.Password) && !string.IsNullOrWhiteSpace(user.Password) && 
+                                                                        !string.IsNullOrWhiteSpace(user.ConfirmPassword))
+                    fieldValidate = "Passwords are different!";
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
